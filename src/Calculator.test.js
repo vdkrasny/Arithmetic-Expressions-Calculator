a0a2e116cc142088c5daec04ac620cc0efcbc981
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-21 12:44:34 +0300

    Added tests for calculateRPNExpression method.

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -11,14 +11,14 @@ describe('Test the Calculator methods.', () => {
                 .toThrow();
         });
 
-        it('Should throw an error if forbidden arithmetic operations were  found in the expression. The available operations: + - * / ( )', () => {
+        it('Should throw an error if unknown arithmetic operator was found in the expression. The available operations: + - * / ( )', () => {
             const wrongInfixExpression = '1 & 2';
 
             expect(() => calculator.convertToRPN(wrongInfixExpression))
                 .toThrow();
         });
 
-        it('Should throw an error if the unary operation was found in the expression', () => {
+        it('Should throw an error if the negative operand was found in the expression', () => {
             const wrongInfixExpression = '1 + -2';
 
             expect(() => calculator.convertToRPN(wrongInfixExpression))
@@ -55,6 +55,57 @@ describe('Test the Calculator methods.', () => {
                 it(`Should converted "${infixExpression}" to "${convertedRPNExpression}"`, () => {
                     expect(calculator.convertToRPN(infixExpression))
                         .toBe(convertedRPNExpression);
+                });
+            });
+        });
+    });
+
+    describe('Testing the calculateRPNExpression method', () => {
+        it('Should throw an error if the expression is not a string', () => {
+            const wrongRPNExpression = 1;
+
+            expect(() => calculator.calculateRPNExpression(wrongRPNExpression))
+                .toThrow();
+        });
+
+        it('Should throw an error if unknown arithmetic operator was found in the expression. The available operations: + - * /', () => {
+            const wrongRPNExpression = '1 2 &';
+
+            expect(() => calculator.calculateRPNExpression(wrongRPNExpression))
+                .toThrow();
+        });
+
+        it('Should throw an error if the negative operand was found in the expression', () => {
+            const wrongRPNExpression = '1 -2 +';
+
+            expect(() => calculator.calculateRPNExpression(wrongRPNExpression))
+                .toThrow();
+        });
+
+        it('Should throw an error if not enough operands were found in the expression', () => {
+            const wrongRPNExpression = '1 2 + -';
+
+            expect(() => calculator.calculateRPNExpression(wrongRPNExpression))
+                .toThrow();
+        });
+
+        it('Should throw an error if the expression exist divide by zero', () => {
+            const wrongRPNExpression = '0 0 /';
+
+            expect(() => calculator.calculateRPNExpression(wrongRPNExpression))
+                .toThrow();
+        });
+
+        describe('Results of the calculation of the expression in the Reverse Polish Notation', () => {
+            const expressions = [
+                ['3 4 2 * 1 5 - / +', '1'],
+                ['1 3 4 + + 2 1 + * 2 * 1 5 - /', '-12']
+            ];
+
+            expressions.forEach(([expressionInRPN, expressionResult]) => {
+                it(`Should return result "${expressionResult}" of "${expressionInRPN}"`, () => {
+                    expect(calculator.calculateRPNExpression(expressionInRPN))
+                        .toBe(expressionResult);
                 });
             });
         });
